3b4a49e08e2189b8f3991bf1c54c1892e93c5e52
pkoniu <patryk.konior@gmail.com>
2015-03-14 00:29:49 +0100

    Added main menu

--- www/js/app.js
+++ www/js/app.js
@@ -30,11 +30,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: "/search",
+  .state('app.main', {
+    url: "/main",
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: "templates/main.html"
       }
     }
   })
