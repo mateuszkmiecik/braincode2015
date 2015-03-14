7f07b7bb24fc1a93111c2ae328accd123115712b
pkoniu <patryk.konior@gmail.com>
2015-03-14 04:02:31 +0100

    test, localStorage

--- www/js/controllers/HomeCtrl.js
+++ www/js/controllers/HomeCtrl.js
@@ -1,6 +1,15 @@
 app.controller('HomeCtrl', function ($scope, CarsService, $timeout, $cordovaVibration) {
     $scope.cars = CarsService.getCars();
     $timeout(function () {
+
+    	$scope.test = '';
+
+    	if(window.localStorage.getItem('test')){
+    		$scope.test = window.localStorage.getItem('test');
+    	}else{
+    		window.localStorage.setItem('test', 'test');
+    	}
+
         $scope.loaded = true;
         if (!!$cordovaVibration) {
             $cordovaVibration.vibrate(100);
