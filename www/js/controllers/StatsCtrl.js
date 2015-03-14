2101ea6cf242290448feccd14e58195d09840eb0
Mateusz Kmiecik <mk.chomik@gmail.com>
2015-03-14 15:21:28 +0100

    begin of charts

--- www/js/controllers/StatsCtrl.js
+++ www/js/controllers/StatsCtrl.js
@@ -1,14 +1,16 @@
 app.controller('StatsCtrl', function($scope, $state, CarsService){
     $scope.fillUps = CarsService.currentCar.fillUps;
 
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
+    $scope.labels = [];
+
+    for(var i = 0; i<$scope.fillUps.length-1; i++){
+        $scope.labels.push($scope.fillUps[i+1].mileage - $scope.fillUps[i].mileage);
+    }
+
+    //$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Średnie spalanie'];
     $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90]
+        [65]
     ];
-    $scope.onClick = function (points, evt) {
-        console.log(points, evt);
-    };
 
 });
